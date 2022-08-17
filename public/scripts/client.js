3a787b08885f35516778edc407a25a5b048d5f62
sylvielcq <slcq.dev@gmail.com>
2022-08-17 15:50:33 -0700

    Add comments to describe the new-tweet submit event listener

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,32 +1,7 @@
-// // Fake data taken from initial-tweets.json
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
-
-
 $(document).ready(() => {
 
+  // Event Listener on the new-tweet submit button: prevents the default behavior.
+  // The input is then serialized into a query string, and sent to the /tweets server page.
   $('.new-tweet').submit((event) => {
 
     event.preventDefault();
