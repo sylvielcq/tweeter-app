9868eb62c13638eb085cf2e426b8d9cda8887662
sylvielcq <slcq.dev@gmail.com>
2022-08-17 13:20:08 -0700

    Put the function declaration back into .ready, as best practices advise

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,24 +1,23 @@
 $(document).ready(() => {
 
+  // Function that updates the characters counter on the New Tweet form.
+  // Takes in no argument. Returns a number.
+  const charCounter = function() {
+
+    const maxChars = 140;
+    let $tweetLength = $(this).val().length;
+    let $charsRemaining = maxChars - $tweetLength;
+    let $counter = $(this).parent().find('.counter').text($charsRemaining);
+
+    if ($charsRemaining < 0) {
+      $($counter).addClass("invalid");
+    }
+
+    if ($charsRemaining > 0) {
+      $($counter).removeClass("invalid");
+    }
+  };
+
   $('#tweet-text').on('input', charCounter);
 
 });
-
-
-// Function that updates the characters counter on the New Tweet form.
-// Takes in no argument. Returns a number.
-const charCounter = function() {
-
-  const maxChars = 140;
-  let $tweetLength = $(this).val().length;
-  let $charsRemaining = maxChars - $tweetLength;
-  let $counter = $(this).parent().find('.counter').text($charsRemaining);
-
-  if ($charsRemaining < 0) {
-    $($counter).addClass("invalid");
-  }
-
-  if ($charsRemaining > 0) {
-    $($counter).removeClass("invalid");
-  }
-};
