955c20d9ec00e50492ff293cc957bd4ba2c8d464
sylvielcq <slcq.dev@gmail.com>
2022-08-16 18:53:18 -0700

    Create event listener and callback function that updates the New Tweet character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,24 @@
 $(() => {
-  $('#tweet-text').on('input', function() {
-    console.log(this.val());
-  })
+
+  $('#tweet-text').on('input', counter);
+
 });
+
+
+// Function that updates the characters counter on the New Tweet form.
+// Takes in no argument. Returns a number.
+const counter = function() {
+
+  const maxChars = 140;
+  let $tweetLength = $(this).val().length;
+  let $charsRemaining = maxChars - $tweetLength;
+  let $counter = $(this).parent().find('.counter').text($charsRemaining);
+
+  if ($charsRemaining < 0) {
+    $($counter).addClass("invalid");
+  }
+
+  if ($charsRemaining > 0) {
+    $($counter).removeClass("invalid");
+  }
+};
