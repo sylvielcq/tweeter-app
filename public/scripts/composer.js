e0a23dd1e3945fd0d77f961dbc41672b458bc7bf
sylvielcq <slcq.dev@gmail.com>
2022-08-17 23:48:39 -0700

    Implement new script for the scroll-up button

---
+++ public/scripts/composer.js
@@ -0,0 +1,28 @@
+$(document).ready(() => {
+
+  $(window).scroll(function() {
+
+    let lastScrollPosition = 0;
+    
+    let currentScrollPosition = $(this).scrollTop();
+    
+    if (currentScrollPosition > lastScrollPosition){
+      $('#scroll-up-btn').addClass('active');
+      $('.nav-new-tweet').removeClass('active');
+    } else {
+      $('#scroll-up-btn').removeClass('active');
+      $('.nav-new-tweet').addClass('active');
+    }
+
+    lastScrollPosition = currentScrollPosition;
+  });
+
+
+  $('#scroll-up-btn').click(function() {
+
+    window.scrollTo(100,100);
+    $('#tweet-text').focus(); // Cursor in textarea
+
+  });
+
+});
