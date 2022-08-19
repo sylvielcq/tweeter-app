afbcb65d118b7d4de608d0811e9f81a91b305164
sylvielcq <slcq.dev@gmail.com>
2022-08-18 20:19:08 -0700

    Add descriptive comments

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -1,15 +1,23 @@
+/* -------------------------------------------------------------------------- */
+/*                              SCROLL UP BUTTON                              */
+/* -------------------------------------------------------------------------- */
+
+
 $(document).ready(() => {
+
+  // Event listener on window scroll
+  // Identifies if the user is scrolling up or down
 
   $(window).scroll(function() {
 
     let lastScrollPosition = 0;
     
-    let currentScrollPosition = $(this).scrollTop();
+    let currentScrollPosition = $(this).scrollTop();       // After scroll position, which is the return value of the scrollTop() function
     
-    if (currentScrollPosition > lastScrollPosition) {
+    if (currentScrollPosition > lastScrollPosition) {      // If user is scrolling down
       $('#scroll-up-btn').addClass('active');
       $('.nav-new-tweet').removeClass('active');
-    } else {
+    } else {                                               // If user is scrolling up
       $('#scroll-up-btn').removeClass('active');
       $('.nav-new-tweet').addClass('active');
     }
@@ -18,10 +26,12 @@ $(document).ready(() => {
   });
 
 
+  // Event listener on the Scroll-Up button
+
   $('#scroll-up-btn').click(function() {
 
-    window.scrollTo(0,0);
-    $('#tweet-text').focus(); // Cursor in textarea
+    window.scrollTo(0,0);                                 // Scroll back to the top of the page
+    $('#tweet-text').focus();                             // Cursor in textarea
 
   });
 
