e3e82618603e5c80028e1b9be787e6154137e77d
sylvielcq <slcq.dev@gmail.com>
2022-08-18 20:07:04 -0700

    Add descriptive comments and separators

--- public/scripts/collapsible.js
+++ public/scripts/collapsible.js
@@ -1,8 +1,15 @@
+/* -------------------------------------------------------------------------- */
+/*                COMPOSE NEW TWEET BUTTON IN NAVIGATION BAR                  */
+/* -------------------------------------------------------------------------- */
+
+
 $(document).ready(() => {
 
   $('#collapsible').click(function() {
-    $('.new-tweet').slideToggle();
-    $('#tweet-text').focus(); // Pointer ready to write a tweet
+    
+    $('.new-tweet').slideToggle();        // Hides/reveals the New Tweet form
+    $('#tweet-text').focus();             // Cursor in textarea
+
   });
 
 });
